6b6cb2ec777adfe5700810ecadd8a5e38f349141
Aluno <Aluno@172.30.1.48>
2019-09-30 20:11:01 -0300

    Primeiro Commit

---
+++ src/telas/TelaJogoForca.java
@@ -0,0 +1,13 @@
+package telas;
+
+import javax.swing.JInternalFrame;
+
+public class TelaJogoForca extends JInternalFrame {
+	
+	public TelaJogoForca() {
+		
+		setSize(400,300);
+		setVisible(true);
+	}
+
+}
---
+++ src/telas/TelaJogoVelha.java
@@ -0,0 +1,15 @@
+package telas;
+
+import javax.swing.JInternalFrame;
+
+public class TelaJogoVelha extends JInternalFrame{
+	public TelaJogoVelha() {
+		super("Jogo da Velha", true, true, true);
+		setSize(400,300);
+		setVisible(true);
+		
+		
+	}
+
+}
+
